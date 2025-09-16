f9ed3be08fe9112283249de495dec3f1a2803243
George Vasiliadis <gvasiliadis72@gmail.com>
2025-09-16 20:07:34 +1000

    feat: fetch with promises.

--- src/index.js
+++ src/index.js
@@ -19,3 +19,24 @@ setTimeout(() => {
 }, 5000);
 
 console.log("Page loaded");
+
+// Make API request to this endpoint to get Pokemon data:
+// https://pokeapi.co/api/v2/pokemon/
+
+function getPokemonData(targetPokemon) {
+    let result = fetch("https://pokeapi.co/api/v2/pokemon/" + targetPokemon).then((response) => {
+        console.log(response);
+        // If the response body contains json data, return that as a valid JSON object.  Just like in the previous flask app.
+        return response.json();
+    }).then((jsonResponse) => {
+        console.log(JSON.stringify(jsonResponse, null, 4));
+        return jsonResponse;
+    });
+
+    console.log(result);
+    setTimeout(() => {
+        console.log(result)
+    }, 2000);
+};
+
+getPokemonData("pikachu");
